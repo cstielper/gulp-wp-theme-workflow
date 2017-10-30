363155077952201a84bccd35324a08592d0c0790
cstielper <chris@mm4solutions.com>
2017-10-30 10:26:03 -0400

    Fix typo in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -18,55 +18,55 @@ browserSync = require('browser-sync').create();
 
 // Spin up a server
 gulp.task('serve', () => {
-browserSync.init({
-	proxy: projectURL
-});
+	browserSync.init({
+		proxy: projectURL
+	});
 });
 
 // PHP/HTML
 // Reload browser on changes to any files
 gulp.task('php', () => {
-gulp.src('./**/*.php').pipe(
-	browserSync.reload({
-		stream: true
-	})
-);
+	gulp.src('./**/*.php').pipe(
+		browserSync.reload({
+			stream: true
+		})
+	);
 });
 
 // Images
 // Optimize images
 gulp.task('imgs', () => {
-return gulp
-	.src('./imgs/*')
-	.pipe(imageOptim.optimize())
-	.pipe(gulp.dest('./imgs/opt'))
-	.pipe(notify({ message: 'TASK: "imgs" completed', onLast: true }));
+	return gulp
+		.src('./imgs/*')
+		.pipe(imageOptim.optimize())
+		.pipe(gulp.dest('./imgs/opt'))
+		.pipe(notify({ message: 'TASK: "imgs" completed', onLast: true }));
 });
 
 // CSS
 // Compile Sass: create source maps, minify output, autoprefix, reload browser
 gulp.task('css', () => {
-gulp
-	.src('./sass/**/*.scss')
-	.pipe(sourcemaps.init())
-	.pipe(
-		sass({
-			outputStyle: cssOutput
-		}).on('error', sass.logError)
-	)
-	.pipe(sourcemaps.write())
-	.pipe(
-		autoprefixer({
-			browsers: ['last 2 versions']
-		})
-	)
-	.pipe(gulp.dest('.'))
-	.pipe(notify({ message: 'TASK: "css" completed', onLast: true }))
-	.pipe(
-		browserSync.reload({
-			stream: true
-		})
-	);
+	gulp
+		.src('./sass/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(
+			sass({
+				outputStyle: cssOutput
+			}).on('error', sass.logError)
+		)
+		.pipe(sourcemaps.write())
+		.pipe(
+			autoprefixer({
+				browsers: ['last 2 versions']
+			})
+		)
+		.pipe(gulp.dest('.'))
+		.pipe(notify({ message: 'TASK: "css" completed', onLast: true }))
+		.pipe(
+			browserSync.reload({
+				stream: true
+			})
+		);
 });
 
 // Javascript
@@ -75,65 +75,65 @@ gulp.task('eslint', () => {
 // So, it's best to have gulp ignore the directory as well.
 // Also, Be sure to return the stream from the task;
 // Otherwise, the task may end before the stream has finished.
-return (
-	gulp
-		.src(['./js/*.js'])
-		// eslint() attaches the lint output to the "eslint" property
-		// of the file object so it can be used by other modules.
-		.pipe(eslint())
-		// eslint.format() outputs the lint results to the console.
-		// Alternatively use eslint.formatEach() (see Docs).
-		.pipe(eslint.format())
-		.pipe(
-			notify({ message: 'TASK: "eslint" completed!', onLast: true })
-		)
-		// To have the process exit with an error code (1) on
-		// lint error, return the stream and pipe to failAfterError last.
-		.pipe(eslint.failAfterError())
-);
+	return (
+		gulp
+			.src(['./js/*.js'])
+			// eslint() attaches the lint output to the "eslint" property
+			// of the file object so it can be used by other modules.
+			.pipe(eslint())
+			// eslint.format() outputs the lint results to the console.
+			// Alternatively use eslint.formatEach() (see Docs).
+			.pipe(eslint.format())
+			.pipe(
+				notify({ message: 'TASK: "eslint" completed!', onLast: true })
+			)
+			// To have the process exit with an error code (1) on
+			// lint error, return the stream and pipe to failAfterError last.
+			.pipe(eslint.failAfterError())
+	);
 });
 
 // Cleanup javascript formatting
 gulp.task('prettier', () => {
-gulp
-	.src('./js/*.js')
-	.pipe(prettier({
-		tabWidth: 2,
-		printWidth: 80,
-		singleQuote: true
-	}))
-	.pipe(gulp.dest('./js'));
+	gulp
+		.src('./js/*.js')
+		.pipe(prettier({
+			tabWidth: 2,
+			printWidth: 80,
+			singleQuote: true
+		}))
+		.pipe(gulp.dest('./js'));
 });
 
 // Compile JS: Transpile with Babel, rename file, minify output, reload browser
 gulp.task('js', () => {
-gulp
-	.src('./js/*.js')
-	.pipe(plumber())
-	.pipe(babel())
-	.pipe(
-		rename({
-			suffix: '.min'
-		})
-	)
-	.pipe(uglify())
-	.pipe(gulp.dest('./js/min'))
-	.pipe(notify({ message: 'TASK: "js" completed', onLast: true }))
-	.pipe(
-		browserSync.reload({
-			stream: true
-		})
-	);
+	gulp
+		.src('./js/*.js')
+		.pipe(plumber())
+		.pipe(babel())
+		.pipe(
+			rename({
+				suffix: '.min'
+			})
+		)
+		.pipe(uglify())
+		.pipe(gulp.dest('./js/min'))
+		.pipe(notify({ message: 'TASK: "js" completed', onLast: true }))
+		.pipe(
+			browserSync.reload({
+				stream: true
+			})
+		);
 });
 
 // Watch for changes and run tasks
 gulp.task('watch', ['serve'], () => {
-//gulp.watch('imgs/*', ['imgs']);
-gulp.watch('sass/**/*.scss', ['css']);
-gulp.watch('js/*.js', ['js']);
-gulp.watch('**/*.php', ['php']);
-//gulp.watch('./src/js/**/*.js', ['eslint']); Linting in code editor. Command can still be run from terminal if you want to lint there
+	//gulp.watch('imgs/*', ['imgs']);
+	gulp.watch('sass/**/*.scss', ['css']);
+	gulp.watch('js/*.js', ['js']);
+	gulp.watch('**/*.php', ['php']);
+	//gulp.watch('./src/js/**/*.js', ['eslint']); Linting in code editor. Command can still be run from terminal if you want to lint there
 });
 
-// Defaul task
+// Default task
 gulp.task('default', ['watch']);
